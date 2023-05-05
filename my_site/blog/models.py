f1180a32148eb6e842a154ccb70b10457d0c6c1b
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-05 10:32:58 -0700

    fixed author- post bug and added tag model see models.py line 5

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 # Create your models here.
 
-
-
-
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -18,4 +17,5 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True,  related_name="posts")
+    tags = models.ManyToManyField(Tag)
