62c01fc029f7cc3eceeabbccb96855cc3eae3035
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-08 11:46:41 -0700

    updated post model to add photo uploads see models.py line 23 and setting.py line 131

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -16,10 +16,11 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
