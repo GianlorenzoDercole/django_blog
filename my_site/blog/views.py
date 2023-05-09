1a2ce35890407476347bd6851b98e44126723dc3
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-09 10:44:26 -0700

    changed home page to class based view see views.py line 29 and urls.py line 7

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
+
+from django.views.generic import ListView
 # Create your views here.
 
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     return render(request, 'blog/index.html', {
+#         "posts": latest_posts
+#     })
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, 'blog/index.html', {
-        "posts": latest_posts
-    })
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts": all_posts
+#     })
+
+# def post_detail(request, slug):
+#     identified_post = Post.objects.get(slug=slug)
+#     return render(request, "blog/post-detail.html", {
+#         "post": identified_post,
+#         "post_tags": identified_post.tags.all()
+#     })
+
+
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
 
 def posts(request):
     all_posts = Post.objects.all().order_by("-date")
