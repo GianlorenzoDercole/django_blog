8d0b02ccb81180ef6dffd2205103775e15fd14f1
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-12 14:24:46 -0700

    rendering stored poses page see views.py line 83 urls.py line 12 sand stored_posts.html line 1

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # path('posts', views.posts, name="posts-page"),
     path('posts',views.AllPostsView.as_view(), name="posts-page"),
     # path('posts/<slug:slug>', views.post_detail, name="post-detail-page") #/posts/my-first-post slug transformer
-    path('posts/<slug:slug>', views.SinglePostView.as_view(), name="post-detail-page" )
+    path('posts/<slug:slug>', views.SinglePostView.as_view(), name="post-detail-page" ),
+    path('read-later', views.ReadLaterView.as_view(), name ="read-later")
 ]
